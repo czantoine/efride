74d67e91ac92ab7b2c0ece43ba910095a8de63c3
czantoine <ac11f@free.fr>
2020-04-11 09:32:33 +0200

    error 404

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" >
+
+<?php include ("head.php"); ?>
+
+<body>
+
+    <?php include ("menu.php"); ?>
+
+   <br/> <br/><br/> 
+
+   <center><h3>ERROR 404</h3></center>
+   <center><a href="efride.php"><p>	Revenir sur le site </p></a></center>
+
+   <center><img src="img/404.gif" alt="EF'Ride"></center>
+
+   <br/><br/>
+     
+
+    <?php include ("footer.php"); ?>
+
+</body>
+</html>
