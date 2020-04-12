d364b60fed97feb65d2775ed1d4cf02736d0a5d0
czantoine <ac11f@free.fr>
2020-04-12 09:44:07 +0200

    Update feed-instagram.php

---
+++ feed-instagram.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr" >
+
+<?php include ("head.php"); ?>
+
+<body>
+
+    <?php include ("menu.php"); ?>
+
+    <br/><br/><br/><br/>       
+
+	<script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/6b558135e4345af48fc5b63fec514ac0.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+    <?php include ("footer.php"); ?>
+
+</body>
+</html>
