2d999afc82c6dae812c6c402bc559ea70f432a74
czantoine <ac11f@free.fr>
2020-06-21 20:05:27 +0200

    Brave footer

--- footer.php
+++ footer.php
@@ -1,39 +1,48 @@
  <footer>
-    <div class="container">
-        <div class="row">
+ 	<div class="container">
+ 		<div class="row">
 
-            <div class="col-md-4">
-                <ul class="list-inline quicklinks">
-                    <li><a href="tips.php">Tips Gratuit</a>
-                    </li>
-                </ul>
-                <p>Copyright © EF'Ride 2014 - 2020</p>
-            </div>
-            <div class="col-md-4">
-                <ul class="list-inline ">
-                    <li><a href="https://www.instagram.com/ef.ride/?hl=fr" target="_blank"><img src="img/insta.png"></a>
-                    </li>
-                    <li><a href="https://www.facebook.com/assos.efride/" target="_blank"><img src="img/fb.png"></a>
-                    </li>
-                    <li><a href="https://www.linkedin.com/company/ef-ride/" target="_blank"><img src="img/linkedin.png"></a>
-                    </li>
-                    <li><a href="https://www.youtube.com/channel/UCTXnvOM8JZJXi9s8JbqtzVg?sub_confirmation=1" target="_blank"><img src="img/youtube.png"></a>
-                    </li>
-                    <li><a href="https://discord.gg/6XkQh93" target="_blank"><img src="img/discord.png"></a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <ul class="list-inline quicklinks">
-                    <li><a href="CGV.php">CGV</a>
-                    </li>
-                </ul>
-                <ul class="list-inline quicklinks">
-                    <li><a href="sitemap.php">Site Map</a>
-                    </li>  
-                </ul>
-            </div>
+ 			<div class="col-md-4">
+ 				<ul class="list-inline quicklinks">
+ 					<li><a href="tips.php">Tips Gratuit</a>
+ 					</li>
+ 				</ul>
+ 				<p>Copyright © EF'Ride 2014 - 2020</p>
+ 				<ul class="list-inline quicklinks">
+ 					<li><a href="pave.php">PAVE</a>
+ 					</li>
+ 				</ul>
+ 			</div>
+ 			<div class="col-md-4">
+ 				<ul class="list-inline ">
+ 					<li><a href="https://www.instagram.com/ef.ride/?hl=fr" target="_blank"><img src="img/insta.png"></a>
+ 					</li>
+ 					<li><a href="https://www.facebook.com/assos.efride/" target="_blank"><img src="img/fb.png"></a>
+ 					</li>
+ 					<li><a href="https://www.linkedin.com/company/ef-ride/" target="_blank"><img src="img/linkedin.png"></a>
+ 					</li>
+ 					<li><a href="https://www.youtube.com/channel/UCTXnvOM8JZJXi9s8JbqtzVg?sub_confirmation=1" target="_blank"><img src="img/youtube.png"></a>
+ 					</li>
+ 					<li><a href="https://discord.gg/6XkQh93" target="_blank"><img src="img/discord.png"></a>
+ 					</li>
+ 				</ul>
+ 			</div>
+ 			<div class="col-md-4">
+ 				<ul class="list-inline quicklinks">
+ 					<li><a href="CGV.php">CGV</a>
+ 					</li>
+ 				</ul>
+ 				<ul class="list-inline quicklinks">
+ 					<li><a href="sitemap.php">Site Map</a>
+ 					</li>  
+ 				</ul>
+ 				<ul class="list-inline quicklinks">
+ 					<li><a href="https://brave.com/efr309"  target="_blank"><img src="img/brave_lion_icon.png"></a>
+ 					</li>
+ 				</ul>
+ 			</div>
 
-        </div>
-    </div>
-</footer>
+
+ 		</div>
+ 	</div>
+ </footer>
