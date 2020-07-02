4a16640f99e4c67e8aad9fce3818a55231a6aded
czantoine <ac11f@free.fr>
2020-07-02 08:28:30 +0200

    photos

--- photos.php
+++ photos.php
@@ -24,29 +24,41 @@
 			<li><img class="lazyload" data-src="img/gallery/session.jpg" loading="lazy" alt="session, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/skate2.jpg" loading="lazy" alt="skate, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/deck1.jpg" loading="lazy" alt="deck, EF'Ride , efride , hock'efrei , hockefrei"/></li> 
+			<li><img class="lazyload" data-src="img/gallery/wei.jpg" loading="lazy" alt="wei, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/chaussures.jpg" loading="lazy" alt="chaussures, EF'Ride , efride , hock'efrei , hockefrei"/></li> 
 			<li><img class="lazyload" data-src="img/gallery/surftrip.jpg" loading="lazy" alt="surftrip, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/session5.jpg" loading="lazy" alt="session, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/paris.jpg" loading="lazy" alt="paris, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/jpo.jpg" loading="lazy" alt="jpo, EFREI, EFREI PARIS, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/kellogs.jpg" loading="lazy" alt="kellogs, EF'Ride , efride , hock'efrei , hockefrei"/></li> 
 			<li><img class="lazyload" data-src="img/gallery/cookie.png" loading="lazy" alt="cookie, EF'Ride , efride , hock'efrei , hockefrei"/></li> 
 			<li><img class="lazyload" data-src="img/gallery/kick.jpg" loading="lazy" alt="kickflip, tricks, EF'Ride , efride , hock'efrei , hockefrei"/></li> 
 			<li><img class="lazyload" data-src="img/gallery/kellogs1.jpg" loading="lazy" alt="kellogs, EF'Ride , efride , hock'efrei , hockefrei"/></li> 
 			<li><img class="lazyload" data-src="img/gallery/montage.jpg" loading="lazy" alt="montage, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/session1.jpg" loading="lazy" alt="session, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/sushi.jpg" loading="lazy" alt="sushi, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/ouch.jpg" loading="lazy" alt="ouch, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/session6.jpg" loading="lazy" alt="session, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/rollerhs.jpg" loading="lazy" alt="roller, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/soiree1.jpg" loading="lazy" alt="soiree, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/skate3.jpg" loading="lazy" alt="skate, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/photo.jpg" loading="lazy" alt="photo, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/juu.jpg" loading="lazy" alt="juu, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/nozman.jpg" loading="lazy" alt="nozman, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/roues.jpg" loading="lazy" alt="roues, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/friend.jpg" loading="lazy" alt="friend, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/session4.jpg" loading="lazy" alt="session, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/petitgad.jpg" loading="lazy" alt="petitgad, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/cornichon.jpg" loading="lazy" alt="skate, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/planche.jpg" loading="lazy" alt="planche, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/deck3.jpg" loading="lazy" alt="deck3, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/spaceedition.jpg" loading="lazy" alt="spaceedition, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/scooter.jpg" loading="lazy" alt="scooter, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/session2.jpg" loading="lazy" alt="session, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/skate1.jpg" loading="lazy" alt="skate, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/trucks.jpg" loading="lazy" alt="trucks, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/supreme.jpg" loading="lazy" alt="deck, EF'Ride , efride , hock'efrei , hockefrei"/></li>
+			<li><img class="lazyload" data-src="img/gallery/session3.jpg" loading="lazy" alt="session, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/soiree.jpg" loading="lazy" alt="soiree, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 			<li><img class="lazyload" data-src="img/gallery/teckdeck.jpg" loading="lazy" alt="teckdeck, EF'Ride , efride , hock'efrei , hockefrei"/></li>
 		</ul>
